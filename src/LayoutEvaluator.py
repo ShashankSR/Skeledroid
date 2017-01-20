9c43708fd4a3b9cf04bfb6407b2f1eb6f2de19a1
ShashankSR <shashanksheshan@gmail.com>
2017-01-21 04:02:45 +0530

    Complete layout evaluator implementation

--- src/LayoutEvaluator.py
+++ src/LayoutEvaluator.py
@@ -16,8 +16,9 @@ def iterate_children(parent):
 
 def parseChild(parent,space):
   for child in iterate_children(parent):
-    parseChild(child,space+1)
     if child.nodeType != child.TEXT_NODE and child.nodeType != child.COMMENT_NODE:
+      customUIClass = ""
+      custom = ""
       if child.tagName == "include":
         layout = child.getAttribute("layout").replace("@layout/","")
         for root, dirs, files in os.walk(pwd+"/main"):
@@ -27,13 +28,34 @@ def parseChild(parent,space):
               parseAndroidXmlDom(fileFullPath,space+1)
               break
       else :
-        print ''*space + child.tagName 
+        if not (child.tagName == "include" or child.tagName == "layout" or child.tagName == "import" or child.tagName == "data" or child.tagName == "variable"):
+          custom = child.tagName.split(".")
+          if len(custom) !=0 :
+            customUIClass = custom[len(custom) - 1]
+            if customUIClass in data:
+              print '  '*space + '<' + data[customUIClass]['extends']
+            else :
+              print '  '*space + '<' + customUIClass
+          else :
+            print '  '*space + '<' +child.tagName 
+          for item in child.attributes.items():
+            print '   '*space + item[0] +"="+ "\"" + item[1] + "\""
+      if not (child.tagName == "include" or child.tagName == "layout" or child.tagName == "import" or child.tagName == "data" or child.tagName == "variable"):
+        print "  "*space + ">"
+      parseChild(child,space+1)
+      if not (child.tagName == "include" or child.tagName == "layout" or child.tagName == "import" or child.tagName == "data" or child.tagName == "variable"):
+        if customUIClass in data:
+          print '  '*space + "</" + data[customUIClass]['extends'] +">"
+        else :
+          print '  '*space + "</" + customUIClass + ">"
+        #print child.attributes.items()
 
 
 def parseAndroidXmlDom(file,space):
   DOMTree = xml.dom.minidom.parse(file)
   collection = DOMTree.documentElement
-  print ''*space + collection.tagName 
+  if not (collection.tagName == "merge" or collection.tagName == "layout"):
+    print '<'*space + collection.tagName 
   parseChild(collection,space)
 
 json_data = open(pwd + "testing/data.json")
